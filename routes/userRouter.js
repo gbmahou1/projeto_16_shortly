09f52d97bd1b3217d3d916968d665e7d798f2783
gbmahou1 <gbmahou1@hotmail.com>
2022-06-10 16:30:40 -0300

    versao final

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { getUsers } from '../controllers/userController.js';
+import { authenticate } from '../middlewares/authenticateMiddleware.js';
+import { getUsers, signUp, signIn, getUsersId, ranking } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/users', getUsers);
+userRouter.post('/signup', signUp);
+userRouter.post('/signin', signIn);
+userRouter.get('/users/:id', authenticate, getUsersId);
+userRouter.get('/ranking', ranking);
 
 export default userRouter;
